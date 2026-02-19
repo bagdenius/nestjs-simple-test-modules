d53a8bab98885deee0c1824be4d2d249b9dc49b9
bagdenius <bagdensparrow@gmail.com>
2026-02-19 16:34:12 +0200

    add jwt config and implement token generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,34 @@
 import { ConflictException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { hash } from 'argon2';
 import { PrismaService } from '../prisma/prisma.service';
 import { SignupRequestDto } from './dto/signup.dto';
-import { hash } from 'argon2';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { StringValue } from 'ms';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  private readonly JWT_SECRET: string;
+  private readonly JWT_ACCESS_TOKEN_TTL: StringValue;
+  private readonly JWT_REFRESH_TOKEN_TTL: StringValue;
+
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.JWT_SECRET = configService.getOrThrow<string>('JWT_SECRET');
+    this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<StringValue>(
+      'JWT_ACCESS_TOKEN_TTL',
+    );
+    this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<StringValue>(
+      'JWT_REFRESH_TOKEN_TTL',
+    );
+  }
 
   async signup(dto: SignupRequestDto) {
     const { name, email, password } = dto;
-
     const existUser = await this.prisma.user.findUnique({
       where: { email },
       select: { email: true },
@@ -18,10 +37,20 @@ export class AuthService {
       throw new ConflictException(
         'User with provided email already registered',
       );
-
-    const user = this.prisma.user.create({
+    const user = await this.prisma.user.create({
       data: { name, email, password: await hash(password) },
     });
-    return user;
+    return this.generateTokens(user.id);
+  }
+
+  private async generateTokens(id: string) {
+    const payload: JwtPayload = { id };
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: this.JWT_ACCESS_TOKEN_TTL,
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: this.JWT_REFRESH_TOKEN_TTL,
+    });
+    return { accessToken, refreshToken };
   }
 }
