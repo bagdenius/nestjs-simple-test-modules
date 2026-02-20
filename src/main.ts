c54f7d77369f30605a811bf33bd8ffb81b20f005
bagdenius <bagdensparrow@gmail.com>
2026-02-21 00:46:53 +0200

    implement custom logger

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { CustomLogger } from './common/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const config = app.get(ConfigService);
   app.use(cookieParser());
+  app.useLogger(new CustomLogger());
   app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   app.enableCors({
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
 
 @Injectable()
 export class TaskService {
+  private readonly logger = new Logger(TaskService.name);
+
   @Cron(CronExpression.EVERY_10_SECONDS)
   handleCron() {
-    console.log('CRON task executes every 10 seconds');
+    this.logger.log('CRON task executes every 10 seconds');
   }
 
   @Interval(1000)
   handleInterval() {
-    console.log('Interval task executes every second');
+    this.logger.log('Interval task executes every second');
   }
 
   @Timeout(5000)
   handleTimeout() {
-    console.log('Timeout task after 5 seconds after start');
+    this.logger.log('Timeout task after 5 seconds after start');
   }
 }
