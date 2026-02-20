ec50b7623965a810567b0d337b6054030373c0f3
bagdenius <bagdensparrow@gmail.com>
2026-02-21 00:14:03 +0200

    add simple cors config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtistModule } from './artist/artist.module';
@@ -13,8 +15,6 @@ import { FileModule } from './file/file.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SpotifyModule } from './spotify/spotify.module';
 import { UserModule } from './user/user.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import path from 'path';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGINS').split(','),
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    exposedHeaders: ['Set-Cookie', 'Content-Disposition'],
+    allowedHeaders: ['Authorization', 'X-Api-Key'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
