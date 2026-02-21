52ffecc6e31ff2e5d41f56392fe74d95f165fbf3
bagdenius <bagdensparrow@gmail.com>
2026-02-22 00:12:38 +0200

    fix index imports

---
+++ src/api/authGql/auth.service.ts
@@ -0,0 +1,119 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { hash, verify } from 'argon2';
+import type { Request, Response } from 'express';
+import ms, { type StringValue } from 'ms';
+import type { JwtPayload } from '../../common/interfaces';
+import { isDev } from '../../common/utils';
+import { PrismaService } from '../../infra/prisma/prisma.service';
+import { LoginInput, SignupInput } from '../auth/inputs';
+
+@Injectable()
+export class AuthService {
+  private readonly JWT_ACCESS_TOKEN_TTL: StringValue;
+  private readonly JWT_REFRESH_TOKEN_TTL: StringValue;
+  private readonly COOKIE_DOMAIN: string;
+
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<StringValue>(
+      'JWT_ACCESS_TOKEN_TTL',
+    );
+    this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<StringValue>(
+      'JWT_REFRESH_TOKEN_TTL',
+    );
+    this.COOKIE_DOMAIN = configService.getOrThrow<string>('COOKIE_DOMAIN');
+  }
+
+  async validate(id: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  private setCookie(response: Response, value: string, expires: Date) {
+    response.cookie('refreshToken', value, {
+      httpOnly: true,
+      domain: this.COOKIE_DOMAIN,
+      expires,
+      secure: !isDev(this.configService),
+      sameSite: isDev(this.configService) ? 'none' : 'lax',
+    });
+  }
+
+  async refresh(request: Request, response: Response) {
+    const refreshToken = request.cookies.refreshToken;
+    if (!refreshToken) throw new UnauthorizedException('Invalid refresh token');
+    const payload: JwtPayload = await this.jwtService.verifyAsync(refreshToken);
+    if (payload) {
+      const user = await this.prisma.user.findUnique({
+        where: { id: payload.id },
+        select: { id: true },
+      });
+      if (!user) throw new NotFoundException('User not found');
+      return this.auth(response, user.id);
+    }
+  }
+
+  private auth(response: Response, id: string) {
+    const { accessToken, refreshToken } = this.generateTokens(id);
+    this.setCookie(
+      response,
+      refreshToken,
+      new Date(Date.now() + ms(this.JWT_REFRESH_TOKEN_TTL)),
+    );
+    return { accessToken };
+  }
+
+  private generateTokens(id: string) {
+    const payload: JwtPayload = { id };
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: this.JWT_ACCESS_TOKEN_TTL,
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: this.JWT_REFRESH_TOKEN_TTL,
+    });
+    return { accessToken, refreshToken };
+  }
+
+  async signup(response: Response, input: SignupInput) {
+    const { name, email, password } = input;
+    const existUser = await this.prisma.user.findUnique({
+      where: { email },
+      select: { email: true },
+    });
+    if (existUser)
+      throw new ConflictException(
+        'User with provided email already registered',
+      );
+    const user = await this.prisma.user.create({
+      data: { name, email, password: await hash(password) },
+    });
+    return this.auth(response, user.id);
+  }
+
+  async login(response: Response, input: LoginInput) {
+    const { email, password } = input;
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      select: { id: true, password: true },
+    });
+    if (!user || !(await verify(user.password, password)))
+      throw new UnauthorizedException('Invalid email or password');
+    return this.auth(response, user.id);
+  }
+
+  async logout(response: Response) {
+    this.setCookie(response, 'refreshToken', new Date(0));
+    return true;
+  }
+}
